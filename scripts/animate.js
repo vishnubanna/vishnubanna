19a435c434ee8d0be9e2bda7c30507af4b47aa4d
vishnubanna <banna3vishnu@gmail.com>
2020-04-16 20:15:39 -0700

    imgs

--- scripts/animate.js
+++ scripts/animate.js
@@ -241,7 +241,7 @@ $(document).ready(function(){
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 $('html, body').animate({
                 scrollTop: $(hash).offset().top
-                }, 200, function(){
+                }, 500, function(){
         
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
